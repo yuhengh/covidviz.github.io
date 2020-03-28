e43ef3fc6ab3f310f4c43f2ccecca76f57d3717f
Johann Huang <johann1111@gmail.com>
2020-03-28 15:23:53 -0700

    initial commit

---
+++ precache-manifest.7be112494f53647ec9048975f528c503.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40260e7b035b2c97568f63c785d2670e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4f2635f44322bb97b507",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1d041d702ff07acc24c8",
+    "url": "/static/js/2.25d1310f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.25d1310f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f2635f44322bb97b507",
+    "url": "/static/js/main.804ee3a9.chunk.js"
+  },
+  {
+    "revision": "eeaa770db05bb3f940a3",
+    "url": "/static/js/runtime-main.4b54536f.js"
+  }
+]);
